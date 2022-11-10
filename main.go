58cb7154917402067b46a174e529ae3917bb6c3b
Alexandre El Khoury <alexandreelkhoury@users-MacBook-Air.local>
2022-11-10 15:13:22 +0100

    third exercice

--- main.go
+++ main.go
@@ -2,40 +2,27 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	//"github.com/joho/godotenv"
 )
 
 func main() {
 
-	fmt.Println("Votre nombre de transactions est par seconde ? Tapez 'seconde' \nVotre nombre de transactions est par minute ? Tapez 'minute'\nVotre nombre de transactions est par heure ? Tapez 'heure'")
-	var temps string
-	fmt.Scanln(&temps)
+	fmt.Println("Combien d'éléments contient votre liste ? ")
+	var nbElements int
+	fmt.Scanln(&nbElements)
 
-	fmt.Println("Quel est votre nombre de transactins ? ")
-	var nbTransactions int
-	fmt.Scanln(&nbTransactions)
+	var liste []string
 
-	var tempsEnSeconde int
-	var tempsEnMinute int
-	var tempsEnHeure int
+	for i := 0; i < nbElements; i++ {
+		fmt.Println("Entrez l'élément", i, " de votre liste : ")
+		var element string
+		fmt.Scanln("%s", &element)
 
-	switch temps {
-	case "seconde":
-		tempsEnSeconde = nbTransactions
-		tempsEnMinute = nbTransactions * 60
-		tempsEnHeure = nbTransactions * 3600
-		fmt.Println("Transactions par seconde : ", tempsEnSeconde, "\n Transactions par minute : ", tempsEnMinute, "\nTransactions par heure : ", tempsEnHeure)
-	case "minute":
-		tempsEnSeconde = nbTransactions / 60
-		tempsEnMinute = nbTransactions
-		tempsEnHeure = nbTransactions * 60
-		fmt.Println("Transactions par seconde : ", tempsEnSeconde, "\n Transactions par minute : ", tempsEnMinute, "\nTransactions par heure : ", tempsEnHeure)
-	case "heure":
-		tempsEnSeconde = nbTransactions / 3600
-		tempsEnMinute = nbTransactions / 60
-		tempsEnHeure = nbTransactions
-		fmt.Println("Transactions par seconde : ", tempsEnSeconde, "\n Transactions par minute : ", tempsEnMinute, "\nTransactions par heure : ", tempsEnHeure)
-	default:
-		fmt.Printf("Veuillez ne pas entrer autre chose que seconde, minute et heure")
+		liste = append(liste, element)
 	}
+
+	sort.Strings(liste)
+	fmt.Println(liste)
+
 }
