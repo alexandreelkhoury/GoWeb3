fd5ef77f5985b23151280db90ddb07b57464a1a1
Alexandre El Khoury <alexandreelkhoury@users-MacBook-Air.local>
2022-11-10 16:07:41 +0100

    exo4

--- exo4/exo4.go
+++ exo4/exo4.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Printf("Bienvenue ! ")
+	resp, err := http.Get("https://api.llama.fi/protocols")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//We Read the response body on the line below.
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//Convert the body to type string
+	sb := string(body)
+	log.Println(sb)
 }
